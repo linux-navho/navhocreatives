149d594be34103ede8ba991f7bdb67d4945ec731
linux-navho <linux.navho@gmail.com>
2024-11-18 13:05:59 +0200

    removed future flags

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,12 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   base: "/navhocreatives/",
   plugins: [
     remix({
       basenaem: "/navhocreatives/",
       ssr: false,
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
     }),
     tsconfigPaths(),
   ],
