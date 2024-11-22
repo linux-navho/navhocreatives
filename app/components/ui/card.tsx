a259126fd83b468bf33d905e761a0f9d3768f1be
linux-navho <linux.navho@gmail.com>
2024-11-22 15:05:07 +0000

    new images

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -11,7 +11,7 @@ const Card = React.forwardRef<
     className={cn(
       "rounded-lg border border-neutral-200 bg-white text-neutral-950 shadow-sm dark:border-neutral-800 dark:bg-neutral-950 dark:text-neutral-50",
       className
-    )}
+    )}         
     {...props}
   />
 ))
